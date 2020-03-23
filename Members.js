c751dcbe906166e102186c4746c1fca3e27925d5
Dinesh <medinu95@gmail.com>
2020-03-22 22:40:52 -0700

    Added a bunch of stuff

--- Members.js
+++ Members.js
@@ -1,24 +1,24 @@
 const members = [   
     {   
-        'id': 0, 
+        'id': "0", 
         'first_name': "Thomas", 
         'last_name': "Wayne", 
         'relation':   'father'
     }, 
     {
-        'id': 1, 
+        'id': "1", 
         'first_name': "Martha", 
         'last_name':    "Wayne",
         'relation':    'mother'       
     }, 
     {
-        'id': 2, 
+        'id': "2", 
         'first_name': "Bruce",  
         'last_name':  "Wayne",  
         'relation':  'youngest son'   
     },
     {
-        'id': 3, 
+        'id': "3", 
         'first_name': "Arthur", 
         'last_name':   "Wayne", 
         'relation':   'oldest son'    
--- routes/api/members.js
+++ routes/api/members.js
@@ -4,6 +4,9 @@ const uuid = require('uuid');
 const members = require('../../Members');
 
 
+// Crud API [create, read, update and delete]
+
+
 // [API] Gets all members
 router.get('/', (req, res)=>{
     res.json(members);
@@ -38,17 +41,18 @@ router.post('/', (req, res)=> {
     members.push(newMember);
 
     res.json(members);
+    //res.redirect('/');
 })
 
 // [API] edits a member by id
 router.put('/:id', (req, res) => {
-    const found = members.some(member => member.id === parseInt(req.params.id));
+    const found = members.some(member => member.id === req.params.id);
 
     if (found){
         const newMember = req.body;
 
         members.forEach(member => {
-            if (member.id === parseInt(req.params.id)){
+            if (member.id === req.params.id){
                 member.first_name = newMember.first_name ? newMember.first_name : member.first_name;
                 member.last_name = newMember.last_name ? newMember.last_name : member.last_name;
                 member.relation = newMember.relation ? newMember.relation : member.relation;
@@ -66,8 +70,13 @@ router.put('/:id', (req, res) => {
 // [API] deletes a member by id(string)
 router.delete('/:id', (req, res)=>{
     const found = members.some(member => member.id === req.params.id);
+    
+    const idx = members.findIndex(obj => obj.id === req.params.id );
+
+    members.splice(idx, 1);
 
     if (found){
+        
         res.json({
             msg: "member has been deleted",
             members: members.filter(member => member.id !== req.params.id)
